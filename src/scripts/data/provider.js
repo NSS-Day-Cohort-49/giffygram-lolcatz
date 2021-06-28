56dae3d5312c0fcbe04d3cb2b03427900d2cbcda
hcrudge <hcrudge1@gmail.com>
2021-06-28 13:49:21 -0500

    initial commit

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -8,5 +8,30 @@ const applicationState = {
         chosenUser: null,
         displayFavorites: false,
         displayMessages: false
-    }
+    },
+    users: [],
+    posts: [],
+    likes: [],
+    messages: []
+}   
+
+
+export const fetchExternalData = () => {
+    return Promise.all([
+    fetch(`${apiURL}/users`)
+    ])
+        .then(responses => {
+            return Promise.all(responses.map(response => {
+                return response.json()
+            }))
+        }) 
+        .then(externalData => {
+            // Store the external state in application state
+            applicationState.users = externalData[0]
+ 
+        })
+}
+
+export const getUsers = () => {
+    return applicationState.users.map(user =>({...user}))
 }
