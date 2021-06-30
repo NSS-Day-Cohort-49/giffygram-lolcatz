af391d22825f23371f336a93f700da87aee1137b
hcrudge <hcrudge1@gmail.com>
2021-06-30 13:36:03 -0500

    Add logout functionality

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,5 +1,7 @@
 export const GiffyGram = () => {
 
     // Show main main UI
-    return `<h1>Giffygram</h1>`
+    return `<h1>Giffygram</h1>
+    <button class="button" id="logoutLink">Log Out</button>`
+
 }
